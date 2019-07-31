002f0b0e319b0668677491f63fe7fd2d347c503d
David <davidsimonkastle@gmail.com>
2019-07-30 23:46:53 -0300

    update: github user request

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,29 @@
 import React, { useState } from 'react';
-
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+
 import { Container, Form, Input, SubmitButton } from './styles';
 
 const Main = () => {
   const [newUser, setNewUser] = useState('');
   const [users, setUsers] = useState([]);
-  const handleAddUser = () => {};
+  const handleAddUser = async () => {
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  };
   return (
     <Container>
       <Form>

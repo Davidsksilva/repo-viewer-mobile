9e711914e50b8b0052d9a5b889d7b75efa08fb87
David <davidsimonkastle@gmail.com>
2019-07-31 00:06:35 -0300

    feature: List github users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,7 +4,19 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import api from '../../services/api';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 const Main = () => {
   const [newUser, setNewUser] = useState('');
@@ -16,7 +28,7 @@ const Main = () => {
       name: response.data.name,
       login: response.data.login,
       bio: response.data.bio,
-      avatar: response.data_url,
+      avatar: response.data.avatar_url,
     };
 
     setUsers([...users, data]);
@@ -39,6 +51,22 @@ const Main = () => {
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
+
+      <List
+        data={users}
+        keyExtractor={user => user.login}
+        renderItem={({ item }) => (
+          <User>
+            <Avatar source={{ uri: item.avatar }} />
+            <Name> {item.name}</Name>
+            <Bio> {item.bio}</Bio>
+
+            <ProfileButton onPress={() => {}}>
+              <ProfileButtonText>Enter profile</ProfileButtonText>
+            </ProfileButton>
+          </User>
+        )}
+      />
     </Container>
   );
 };

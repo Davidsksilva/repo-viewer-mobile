c718b992d26cdfcbce11dcb35816502a032de793
David <davidsimonkastle@gmail.com>
2019-08-06 22:53:11 -0300

    feature: activity indicator on star fetching

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
+import { ActivityIndicator } from 'react-native';
 import {
   Container,
   Header,
@@ -13,17 +13,20 @@ import {
   Info,
   Title,
   Author,
+  LoadingContainer,
 } from './styles';
 import api from '../../services/api';
 
 const User = ({ navigation }) => {
   const [stars, setStars] = useState([]);
+  const [loading, setLoading] = useState(false);
   // Get list of starred repositories
   async function fetchStarredRepos() {
+    setLoading(true);
     const user = navigation.getParam('user');
 
     const response = await api.get(`/users/${user.login}/starred`);
-
+    setLoading(false);
     setStars(response.data);
   }
 
@@ -39,20 +42,25 @@ const User = ({ navigation }) => {
         <Name> {user.name}</Name>
         <Bio> {user.bio}</Bio>
       </Header>
-
-      <Stars
-        data={stars}
-        keyExtractor={star => String(star.id)}
-        renderItem={({ item }) => (
-          <Starred>
-            <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-            <Info>
-              <Title>{item.name}</Title>
-              <Author>{item.owner.login}</Author>
-            </Info>
-          </Starred>
-        )}
-      />
+      {loading ? (
+        <LoadingContainer>
+          <ActivityIndicator size="large" color="#189ff0" />
+        </LoadingContainer>
+      ) : (
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      )}
     </Container>
   );
 };
--- src/pages/User/styles.js
+++ src/pages/User/styles.js
@@ -72,3 +72,9 @@ export const Author = styled.Text`
   color: #666;
   margin-top: 2px;
 `;
+
+export const LoadingContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;

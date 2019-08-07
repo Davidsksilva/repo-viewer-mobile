0d2f026873cbce1b4ad7ccfd0215e3dab0bc13f7
David <davidsimonkastle@gmail.com>
2019-08-06 22:48:55 -0300

    feature: listing stars

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,70 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
+import api from '../../services/api';
 
 const User = ({ navigation }) => {
+  const [stars, setStars] = useState([]);
+  // Get list of starred repositories
+  async function fetchStarredRepos() {
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    setStars(response.data);
+  }
+
+  useEffect(() => {
+    fetchStarredRepos();
+  }, []);
+
+  const user = navigation.getParam('user');
   return (
-    <View>
-      <Text />
-    </View>
+    <Container>
+      <Header>
+        <Avatar source={{ uri: user.avatar }} />
+        <Name> {user.name}</Name>
+        <Bio> {user.bio}</Bio>
+      </Header>
+
+      <Stars
+        data={stars}
+        keyExtractor={star => String(star.id)}
+        renderItem={({ item }) => (
+          <Starred>
+            <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+            <Info>
+              <Title>{item.name}</Title>
+              <Author>{item.owner.login}</Author>
+            </Info>
+          </Starred>
+        )}
+      />
+    </Container>
   );
+};
+
+User.navigationOptions = ({ navigation }) => ({
+  title: navigation.getParam('user').name,
+});
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
 };
 
 export default User;
